d64c36163fa71149ba787b19c3f9578997720d8d
Tak <levi@unity3d.com>
2011-08-26 18:40:51 +0200

    Enable setting editor font.

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.OptionPanels/GeneralOptionsPanel.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.OptionPanels/GeneralOptionsPanel.cs
@@ -57,6 +57,8 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 			this.enableParameterInsightCheckbutton.Active = DefaultSourceEditorOptions.Instance.EnableParameterInsight;
 			this.hideObsoleteItemsCheckbutton.Active = DefaultSourceEditorOptions.Instance.HideObsoleteItems;
 			this.autoCodeCompletionCheckbutton.Active = DefaultSourceEditorOptions.Instance.EnableAutoCodeCompletion;
+			this.fontselection.SetFontName (DefaultSourceEditorOptions.Instance.FontName);
+			radiobutton2.Active = !(radiobutton1.Active = this.fontselection.FontName == MonoDevelop.Ide.DesktopService.DefaultMonospaceFont);
 			
 			HandleCodeCompletioncheckbuttonToggled (this, EventArgs.Empty);
 			return this;
@@ -72,6 +74,9 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 			DefaultSourceEditorOptions.Instance.CompleteWithSpaceOrPunctuation = this.spaceOrPunctiuationCheckbutton.Active;
 			DefaultSourceEditorOptions.Instance.EnableParameterInsight = this.enableParameterInsightCheckbutton.Active;
 			DefaultSourceEditorOptions.Instance.HideObsoleteItems = this.hideObsoleteItemsCheckbutton.Active;
+			if (this.radiobutton1.Active)
+				MonoDevelop.Ide.Fonts.FontService.SetFont("Editor", MonoDevelop.Ide.DesktopService.DefaultMonospaceFont);
+			else MonoDevelop.Ide.Fonts.FontService.SetFont ("Editor", this.fontselection.FontName);
 		}
 
 		public void Initialize (OptionsDialog dialog, object dataObject)
